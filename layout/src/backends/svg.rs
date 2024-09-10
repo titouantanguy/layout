ea11ddbaa6e101336b6c7068b6848d95fbc1e50c
Titouan Tanguy <titouan.tanguy@zama.ai>
2024-09-10 17:15:55 +0200

    Do not grow window too much

--- layout/src/backends/svg.rs
+++ layout/src/backends/svg.rs
@@ -129,10 +129,7 @@ impl SVGWriter {
         let svg_line = format!(
             "<svg width=\"{}\" height=\"{}\" viewBox=\"0 0 {} {}\
             \" xmlns=\"http://www.w3.org/2000/svg\">\n",
-            self.view_size.x + 5.,
-            self.view_size.y + 5.,
-            self.view_size.x + 5.,
-            self.view_size.y + 5.
+            5., 5., 5., 5.
         );
         result.push_str(&svg_line);
         result.push_str(SVG_DEFS);
